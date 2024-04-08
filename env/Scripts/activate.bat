e5353863590752bd5af1b532f1b975d8fe20e578
Dallas <dkea201@mywhitecliffe.com>
2024-04-09 11:02:08 +1200

    Tweaked directory

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jmant\OneDrive\Documents\Recip\env
+set VIRTUAL_ENV=C:\Users\hotsp\Documents\Github Repos\Recip\env
 
 if not defined PROMPT set PROMPT=$P$G
 
