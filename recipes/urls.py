641934e937610924d193c107144290fe066550d8
Justin Manuel <112037970+J-Manuel-NZ@users.noreply.github.com>
2024-04-17 10:19:53 +1200

    Added the Create Recipe feature
    + Added a create recipe form
    + Users can add recipes linked to their accounts
    + Added Success message

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeUpdateView, RecipeDeleteView, HomeView, AboutView, get_random_recipe
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeCreateSuccessView, RecipeUpdateView, RecipeDeleteView, HomeView, AboutView, get_random_recipe
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -8,8 +8,7 @@ urlpatterns = [
     path('recipes/<str:category_name>/', RecipeListView.as_view(), name="recipes_list_by_category"),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
     path('recipe/random/', get_random_recipe, name="random_recipe"),
-    # path('recipes/create/', RecipeCreateView.as_view(), name="recipes-create"),
-    # path('recipe/<int:pk>/update/', RecipeUpdateView.as_view(), name="recipes-update"),
-    # path('recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name="recipes-delete"),
+    path('recipes/create/create/', RecipeCreateView.as_view(), name="create_recipe"),
+    path('recipes/create/success/', RecipeCreateSuccessView.as_view(), name="create_recipe_success"),
     path('about/', AboutView.as_view(), name='about'),
 ]
